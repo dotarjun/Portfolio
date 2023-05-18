54b3da6e76396363f35bc1272299fd78406adb0c
Arjun <arjunsingh8112@gmail.com>
2023-05-18 13:32:01 +0530

    🎨 style(Logo.js): update Logo component to use Next.js Link component and add styles

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Logo = () => {
     return (
-        <div>Logo</div>
+        <div className='flex items-center justify-center mt-2'>
+            <Link
+                href="/"
+                className='w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold'>
+                AS
+            </Link>
+        </div>
     )
 }
 
