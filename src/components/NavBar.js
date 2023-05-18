cd7eca6915a467ac7beb12347ac335c666a67354
Arjun <arjunsingh8112@gmail.com>
2023-05-18 13:28:55 +0530

    🎨 style(NavBar.js): add navigation links and logo to the NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,27 @@
+import Link from 'next/link'
 import React from 'react'
+import Logo from './Logo'
 
 const NavBar = () => {
     return (
-        <div>NavBar</div>
+        <header
+            className='w-full px-32 py-8 font-medium flex items-center justify-between'
+        >
+            <nav>
+                <Link href="/">Home</Link>
+                <Link href="/">About</Link>
+                <Link href="/">Projects</Link>
+                <Link href="/">Articles</Link>
+            </nav>
+            <Logo />
+            <nav>
+                <Link href="/" target={"_blank"}>Twitter</Link>
+                <Link href="/" target={"_blank"}>Twitter</Link>
+                <Link href="/" target={"_blank"}>Twitter</Link>
+                <Link href="/" target={"_blank"}>Twitter</Link>
+                <Link href="/" target={"_blank"}>Twitter</Link>
+            </nav>
+        </header>
     )
 }
 
