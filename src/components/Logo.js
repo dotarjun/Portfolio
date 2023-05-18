b01c39a031a5b014cf098409fcb4ede19ba5cc5f
Arjun <arjunsingh8112@gmail.com>
2023-05-18 13:36:29 +0530

    🎨 style(Logo.js): add framer-motion to Logo component and add hover effect to logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,19 @@
 import Link from 'next/link'
 import React from 'react'
+import { motion } from "framer-motion";
+
+const MotionLink = motion(Link);
 
 const Logo = () => {
     return (
         <div className='flex items-center justify-center mt-2'>
-            <Link
+            <MotionLink
                 href="/"
-                className='w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold'>
+                className='w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold'
+                whileHover={{ scale: 1.5 }}>
                 AS
-            </Link>
-        </div>
+            </MotionLink>
+        </div >
     )
 }
 
