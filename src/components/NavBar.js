3c2c5601cc5aa0cedf55a9a83fa79f09fefacc4b
Arjun <arjunsingh8112@gmail.com>
2023-05-18 14:40:10 +0530

    🎨 style(NavBar.js): improve CustomLink component readability and add active link style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,21 @@
 import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
+import { useRouter } from 'next/router'
 
 const CustomLink = ({ href, title, className = "" }) => {
+    const router = useRouter();
     return (
         <Link href={href} className={`${className} relative group`}>
             {title}
-            <span className='h-[1px] inline-block w-full bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300'>&nbsp;</span>
+            <span className={`
+            h-[1px] inline-block bg-dark
+            absolute left-0 -bottom-0.5
+            group-hover:w-full transition-[width] ease duration-300
+            ${router.asPath === href ? 'w-full' : 'w-0'}
+            `}>
+                &nbsp;
+            </span>
         </Link>
     )
 }
