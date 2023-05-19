62c236aec140554994629329a393b597ce185efb
Arjun <arjunsingh8112@gmail.com>
2023-05-19 11:42:57 +0530

    ✨ feat(AnimatedText.js): add AnimatedText component to the project

---
+++ src/components/AnimatedText.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AnimatedText = ({ text, className = "" }) => {
+  return (
+    <div
+      className="w-full mx-auto py-2 flex items-center justify-center text-center
+    overflow-hidden">
+          <h1 className={` ${className}`}>
+              
+          </h1>
+      AnimatedText
+    </div>
+  );
+};
+
+export default AnimatedText;
