e2ee37053846466a69e015e8cd2726d2e69226b5
Arjun <arjunsingh8112@gmail.com>
2023-05-19 11:54:40 +0530

    🎨 style(AnimatedText.js): refactor AnimatedText component to use inline-block for text and add whitespace between words

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -4,11 +4,17 @@ const AnimatedText = ({ text, className = "" }) => {
   return (
     <div
       className="w-full mx-auto py-2 flex items-center justify-center text-center
-    overflow-hidden">
-          <h1 className={` ${className}`}>
-              
-          </h1>
-      AnimatedText
+    overflow-hidden"
+    >
+      <h1
+        className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className}`}
+      >
+        {text.split(" ").map((word, index) => (
+          <span key={word + "-" + index} className="inline-block">
+            {word}&nbsp;
+          </span>
+        ))}
+      </h1>
     </div>
   );
 };
