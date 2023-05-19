2c81b828be28c9b273a192309429c04aa5948578
Arjun <arjunsingh8112@gmail.com>
2023-05-19 15:48:59 +0530

    ✨ feat(components): add Footer component 🎨 style(_app.js): add Footer component to main layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,13 @@
-import NavBar from '@/components/NavBar';
-import '@/styles/globals.css'
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
-import Head from 'next/head';
+import Head from "next/head";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
-  variable: "--font-mont"
-})
-
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }) {
   return (
@@ -16,10 +16,13 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBar />
-      <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
+        <NavBar />
         <Component {...pageProps} />
+        <Footer />
       </main>
     </>
-  )
+  );
 }
