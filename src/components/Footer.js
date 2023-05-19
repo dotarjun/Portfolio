f73a4704f42690677cb2ce9b3d6821f55c5e4932
Arjun <arjunsingh8112@gmail.com>
2023-05-19 16:17:03 +0530

    🎨 style(Footer.js): add footer component with copyright and author information 🚀 chore(Footer.js): add link to contact page in footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Layout from "./Layout";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="w-full border-t-2 border-dark border-solid font-medium text-lg">
+      <Layout className="py-8 flex items-center justify-between">
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
+        <div className="flex items-center">
+          Built with <span className="text-dark text-2xl px-1">❤</span> by&nbsp;
+          <Link href="https://github.com/dotarjun" className="text-primary ">
+            dotarjun
+          </Link>
+        </div>
+        <Link href="/">Contact</Link>
+      </Layout>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
