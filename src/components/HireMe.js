3fda69235687529e7789a7f17944839317b86dee
Arjun <arjunsingh8112@gmail.com>
2023-05-20 11:43:49 +0530

    🎨 style(HireMe.js): increase size of Hire Me button to 6.5rem and add text wrap to center text. Also add hover effect to scale and change font size to xl.

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -8,7 +8,7 @@ const HireMe = () => {
   return (
     <div className="fixed right-5 bottom-5 flex items-center justify-center overflow-hidden">
       <motion.div
-        className=" w-52 h-auto flex items-center justify-center relative"
+        className=" w-56 h-auto flex items-center justify-center relative"
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5 }}
@@ -20,9 +20,9 @@ const HireMe = () => {
           absolute left-1/2 top-1/2 
           -translate-x-1/2 -translate-y-1/2 bg-dark text-light
           shadow-md border-solid 
-           w-24 h-24 rounded-full font-semibold
+           w-[6.5rem] h-[6.5rem] rounded-full font-semibold
           hover:bg-light hover:text-dark
-          hover:scale-110
+          hover:scale-110 text-xl flex-wrap text-center
           "
         >
           Hire Me
