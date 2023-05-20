c92bb42b8b3147ef908587e0b17a8876eae1823c
Arjun <arjunsingh8112@gmail.com>
2023-05-20 12:32:54 +0530

    🎨 style(Badges.js): add new badges for ESLint, Vite, and Linux

--- src/components/Badges.js
+++ src/components/Badges.js
@@ -23,11 +23,20 @@ const Badges = ({ className }) => {
       <span class="bg-red-100 text-red-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
         Git
       </span>
-      <span class="bg-teal-100 text-teal-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">
+      <span class="bg-cyan-100 text-cyan-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">
         Tailwind
       </span>
-      <span class="bg-red-100 text-red-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
+      <span class="bg-rose-100 text-rose-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
         Jest
+      </span>
+      <span class="bg-indigo-100 text-indigo-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-purple-900 dark:text-purple-300">
+        ESLint
+      </span>
+      <span class="bg-purple-100 text-purple-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+        Vite
+      </span>
+      <span class="bg-orange-100 text-orange-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+        Linux
       </span>
     </div>
   );
