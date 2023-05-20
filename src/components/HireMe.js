e2b99ac65cda482123b37a8b1606138eb13a69b1
Arjun <arjunsingh8112@gmail.com>
2023-05-20 11:13:05 +0530

    🎨 style(HireMe.js): add framer-motion animation to Hire Me button 🐛 fix(HireMe.js): adjust Hire Me button size to fit text better

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -2,11 +2,17 @@ import React from "react";
 import { CircularText } from "./Icons";
 import Link from "next/link";
 import MailTo from "./MailTo";
+import { motion } from "framer-motion";
 
 const HireMe = () => {
   return (
-    <div className="fixed right-4 bottom-4 flex items-center justify-center overflow-hidden">
-      <div className="w-48 h-auto flex items-center justify-center relative">
+    <div className="fixed right-5 bottom-5 flex items-center justify-center overflow-hidden">
+      <motion.div
+        className=" w-52 h-auto flex items-center justify-center relative"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+      >
         <CircularText className={"fill-dark  animate-spin-slow"} />
         <Link
           href={`${MailTo()}`}
@@ -14,14 +20,14 @@ const HireMe = () => {
           absolute left-1/2 top-1/2 
           -translate-x-1/2 -translate-y-1/2 bg-dark text-light
           shadow-md border-solid 
-          w-20 h-20 rounded-full font-semibold
+           w-24 h-24 rounded-full font-semibold
           hover:bg-light hover:text-dark
-          hover:scale-110 
+          hover:scale-110
           "
         >
           Hire Me
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 };
