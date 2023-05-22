d1c1df74963b974f8259729d98ca8506643fb66a
Arjun <arjunsingh8112@gmail.com>
2023-05-22 12:21:00 +0530

    ✨ feat(Skills.js): create Skills component with a title and a container for skills.

---
+++ src/components/Skills.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div>
+      <h2 className="font-bold text-6xl mt-64 w-full text-center">Skills</h2>
+      <div className="w-full h-screen relative flex items-center justify-center rounded-full">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
