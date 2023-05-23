05552e3591e4857fb079ffef531a7d5a0b0571bf
Arjun <arjunsingh8112@gmail.com>
2023-05-23 11:22:12 +0530

    🎨 style(Skills.js): adjust margin-top of Skills title to 32px

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,10 +3,8 @@ import React from "react";
 const Skills = () => {
   return (
     <div>
-      <h2 className="font-bold text-6xl mt-64 w-full text-center">Skills</h2>
-      <div className="w-full h-screen relative flex items-center justify-center rounded-full">
-        
-      </div>
+      <h2 className="font-bold text-6xl mt-32 w-full text-center">Skills</h2>
+      <div className="w-full h-screen relative flex items-center justify-center rounded-full"></div>
     </div>
   );
 };
