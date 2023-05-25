e7bbb17f0c31b98fc87171fd8c975c2fa1363dcb
Arjun <arjunsingh8112@gmail.com>
2023-05-25 12:55:35 +0530

    🎨 style(Skills.js): add SkillsCard component and display skills in a grid

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,20 @@
 import React from "react";
+import SkillsCard from "./SkillsCard";
 
 const Skills = () => {
   return (
     <div>
       <h2 className="font-bold text-6xl mt-32 w-full text-center">Skills</h2>
-      <div className="w-full h-screen relative flex items-center justify-center rounded-full"></div>
+      <div className="w-full h-screen relative flex items-center justify-center rounded-full">
+        <div className="flex flex-wrap gap-4">
+          <SkillsCard name="react" />
+          <SkillsCard name="nextjs" />
+          <SkillsCard name="mysql" />
+          <SkillsCard name="tailwindcss" />
+          <SkillsCard name="jest" />
+          <SkillsCard name="mysql" />
+        </div>
+      </div>
     </div>
   );
 };
