d70a97df6d5c1bfedcc0467d4aa533c8e85eca97
Arjun <arjunsingh8112@gmail.com>
2023-05-29 17:00:55 +0530

    🎨 style(Experience.js): reduce top and bottom margin of Experience component by half

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Experience = () => {
   return (
-    <div className="my-64">
+    <div className="my-32">
       <h2 className="font-bold text-5xl mb-32 w-full text-center">
         Experience
       </h2>
