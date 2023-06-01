7a51cd86df69aa141772569ec6ef0adb6cd9bdd3
Arjun <arjunsingh8112@gmail.com>
2023-06-01 20:09:29 +0530

    🐛 fix(Experience.js): add missing props to Details component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,11 +1,39 @@
 import React from "react";
 
+const Details = ({
+  position,
+  company,
+  companyLink,
+  time,
+  address,
+  description,
+}) => {
+  return (
+    <li>
+      <div>
+        <h3>
+          {position}&nbsp;<a href={companyLink}>, {company}</a>
+        </h3>
+        <span>
+          {time}|{address}
+        </span>
+        <p>{description}</p>
+      </div>
+    </li>
+  );
+};
+
 const Experience = () => {
   return (
     <div className="my-32">
       <h2 className="font-bold text-5xl mb-32 w-full text-center">
         Experience
       </h2>
+      <div className=" w-[75%] mx-auto relative">
+        <ul>
+          <Details />
+        </ul>
+      </div>
     </div>
   );
 };
